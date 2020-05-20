248210f2ea64783d44d5f15ed5c58640baf0af7b
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-05-20 15:30:19 +0100

    Can view all guests

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Hotel Costa Del Sol Bookings</h1>
+    <booking-list> </booking-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BookingList from './components/BookingList.vue';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    'booking-list': BookingList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ client/src/components/BookingList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(booking, index) in bookings" :key="index"> 
+                Name: {{ booking.name }} 
+                <ul>
+                    <li>Email: {{ booking.email}} </li>
+                    <li>Check-in status: {{ booking.checked_in }}</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import BookingService from '../services/BookingService.js'
+
+export default {
+    name: 'booking-list',
+    data: function(){
+        return {
+            bookings: []
+        }
+    },
+    mounted: function(){
+       BookingService.getBookings()
+       .then(bookings => this.bookings = bookings); 
+    }
+}
+</script>
+
+<style>
+
+</style>
