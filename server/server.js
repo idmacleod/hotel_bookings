632a7baa77d22f93e183dedd7a4b459ebf1e7843
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-05-20 13:41:04 +0100

    Added Index route

--- server/server.js
+++ server/server.js
@@ -6,3 +6,21 @@ const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017')
+    .then((client) => {
+        const db = client.db('hotel');
+        const bookingsCollection = db.collection('bookings');
+        const bookingsRouter = createRouter(bookingsCollection);
+        app.use('/api/bookings', bookingsRouter);
+    })
+    .catch(console.err);
+
+app.listen(3000, function() {
+    console.log('Server listening on port 3000');
+});
+
+
+
