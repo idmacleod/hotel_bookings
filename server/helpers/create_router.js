77fdb687085f88440ad87cd0a83ba3f35f80755c
idmacleod <idmacleod@gmail.com>
2020-05-20 14:54:18 +0100

    Added CREATE, UPDATE and DESTROY routes

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -25,14 +25,58 @@ const createRouter = function(collection) {
             .findOne({ _id: ObjectID(id) })
             .then((doc) => res.json(doc))
             .catch((err) => {
-            console.error(err);
-            res.status(500);
-            res.json({ status: 500, error: err });
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err });
         });
     });
 
+    // CREATE
+    router.post('/', (req, res) => {
+        const newData = req.body;
+        collection
+            .insertOne(newData)
+            .then(result => res.json(result.ops[0]))
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err });
+            });
+    });
+
+    // UPDATE
+    router.put('/:id', (req, res) => {
+        const id = req.params.id;
+        const updatedData = req.body;
+        collection
+            .findOneAndUpdate(
+                { _id: ObjectID(id) },
+                { $set: updatedData },
+                { returnOriginal: false }
+            )
+            .then(result => res.json(result.value))
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err });
+            });
+    });
+
+    // DESTROY
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id;
+        collection
+            .deleteOne({ _id: ObjectID(id) })
+            .then(result => res.json(result))
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err });
+            });
+    });
 
     return router;
+
 };
 
 module.exports = createRouter;
