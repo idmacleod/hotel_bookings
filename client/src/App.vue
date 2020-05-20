8f08cc6cd0ec960479641a5327a57da34b595e21
idmacleod <idmacleod@gmail.com>
2020-05-20 15:49:49 +0100

    Added form, need to write service

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <h1>Hotel Costa Del Sol Bookings</h1>
+    <booking-form> </booking-form>
     <booking-list> </booking-list>
   </div>
 </template>
 
 <script>
 import BookingList from './components/BookingList.vue';
+import BookingForm from './components/BookingForm.vue';
 
 export default {
   name: 'app',
   components: {
-    'booking-list': BookingList
+    'booking-list': BookingList,
+    'booking-form': BookingForm
   }
 }
 </script>
---
+++ client/src/components/BookingForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <form v-on:submit.prevent="addBooking">
+        <label for="name">Guest Name:</label>
+        <input type="text" name="name" id="name" v-model="name">
+        <label for="email">Email Address:</label>
+        <input type="email" name="email" id="email" v-model="email">
+        <input type="checkbox" name="checked_in" id="checked_in" v-model="checked_in">
+        <label for="checked_in">Checked In</label>
+        <input type="submit" value="Add Booking">
+      </form>
+  </div>
+</template>
+
+<script>
+import BookingService from '../services/BookingService.js';
+
+export default {
+    name: 'booking-form',
+    data: function () {
+        return {
+            name: "",
+            email: "",
+            checked_in: false
+        }
+    },
+    methods: {
+        addBooking: function () {
+            const newBooking = {
+                name: this.name,
+                email: this.email,
+                check_in: this.checked_in
+            }
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
